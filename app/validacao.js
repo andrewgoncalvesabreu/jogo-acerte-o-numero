aba158a8fb679e64919b3c76f2f40bc578ded67c
Andrew Gonçalves Abreu <124524087+andrewgoncalvesabreu@users.noreply.github.com>
2023-08-13 15:33:00 -0300

    Projeto Finalizado!

--- app/validacao.js
+++ app/validacao.js
@@ -1,4 +1,19 @@
 function verificaSeOChutePossuiUmValorValido(chute) {
+
+    if (chute.toUpperCase() === "GAME OVER") {
+
+        document.body.innerHTML =
+            `
+            <h2>Game Over!!!</h2>
+            <h3>Pressione o botão para jogar novamente</h3>
+            <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+            `
+            document.body.style.backgroundColor = "black";
+    } else {
+
+        elementoChute.innerHTML += '<div>Valor Inválido</div>';
+    }
+    
     const numero = +chute
 
     if (chuteForInvalido(numero)) {
